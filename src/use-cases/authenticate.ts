0e8259f9577e19b2468f51e91e197b56f19378fe
Stéfany Larissa <stelar.dev@gmail.com>
2023-11-18 13:51:31 -0300

    adding authenticate use case

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,33 @@
+import { type UsersRepository } from '@/repositories/users-repository'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { type User } from '@prisma/client'
+import { compare } from 'bcryptjs'
+
+interface AuthenticaseUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticaseUseCaseResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor (private readonly usersRepository: UsersRepository) {}
+
+  async execute ({ email, password }: AuthenticaseUseCaseRequest): Promise<AuthenticaseUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (user === null) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatch = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatch) {
+      throw new InvalidCredentialsError()
+    }
+
+    return { user }
+  }
+}
