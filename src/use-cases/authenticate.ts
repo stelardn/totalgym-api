4e7aece45eebcf59b812a8df7eda0111b3d90a2d
Stéfany Larissa <stelar.dev@gmail.com>
2023-11-20 19:04:03 -0300

    fix: authenticate typo

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -3,19 +3,19 @@ import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 import { type User } from '@prisma/client'
 import { compare } from 'bcryptjs'
 
-interface AuthenticaseUseCaseRequest {
+interface AuthenticateUseCaseRequest {
   email: string
   password: string
 }
 
-interface AuthenticaseUseCaseResponse {
+interface AuthenticateUseCaseResponse {
   user: User
 }
 
 export class AuthenticateUseCase {
   constructor (private readonly usersRepository: UsersRepository) {}
 
-  async execute ({ email, password }: AuthenticaseUseCaseRequest): Promise<AuthenticaseUseCaseResponse> {
+  async execute ({ email, password }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
     const user = await this.usersRepository.findByEmail(email)
 
     if (user === null) {
