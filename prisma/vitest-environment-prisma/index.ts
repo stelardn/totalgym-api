5f1f48b6542a13d55cfcd658ee743bb7fb16322d
Stéfany Larissa <stelar.dev@gmail.com>
2024-01-23 21:07:41 -0300

    setup: prisma test environment

--- prisma/vitest-environment-prisma/index.ts
+++ prisma/vitest-environment-prisma/index.ts
@@ -1,15 +1,33 @@
+import 'dotenv/config'
+import { randomUUID } from 'crypto'
 import { type Environment } from 'vitest'
+import { execSync } from 'node:child_process'
+import { prisma } from '@/lib/prisma'
+
+function generateDatabaseURL (schema: string) {
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup () {
-    console.log('setup')
+    const schema = randomUUID()
+
+    process.env.DATABASE_URL = generateDatabaseURL(schema)
+
+    execSync('yarn prisma migrate deploy')
 
     return {
-      teardown () {
-        console.log('teardown')
+      async teardown () {
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`)
+
+        await prisma.$disconnect()
       }
     }
   }
